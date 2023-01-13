0d9b8ffe99951eadec688b7dfd9f10dfc7a42099
Nick Lalic <nick.lalic@gmail.com>
2023-01-13 10:04:02 -0700

    🐛 fixed bugs in Fsw: Fsw.channels should return names; Fsw.create_channel should not switch channel name, type

--- project/fsw.py
+++ project/fsw.py
@@ -5,12 +5,12 @@ class Fsw(GenericInstrument):
 
     @property
     def channels(self):
-        result = self.query(':INST:LIST?')
+        result = self.query(':INST:LIST?').strip()
         parts  = result.replace("'", '').split(',')
-        return parts[::1]  # names
+        return parts[1::2]  # names
 
     def create_channel(self, name, type='SAN'):
-        self.write(f"INST:CRE {name}, '{type}'")
+        self.write(f"INST:CRE {type}, '{name}'")
 
     def delete_channel(self, name):
         self.write(f"INST:DEL '{name}'")
